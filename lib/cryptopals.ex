fc3e8ccf80d29a159ff4ac05cc822c80741bee75
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2025-03-14 16:52:52 -0400

    mix new cryptopals

---
+++ lib/cryptopals.ex
@@ -0,0 +1,22 @@
+defmodule Cryptopals do
+  @moduledoc """
+  Documentation for `Cryptopals`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cryptopals.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def main(args) do
+    IO.puts "Hello, world!"
+  end
+end
