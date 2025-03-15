e7dc474df2902e6e8012e2eb75243758b9b1d0a6
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2025-03-15 09:04:48 -0400

    Cryptopals: Set 1, Challenge 1

--- lib/cryptopals.ex
+++ lib/cryptopals.ex
@@ -4,19 +4,44 @@ defmodule Cryptopals do
   """
 
   @doc """
-  Hello world.
+  Convert a hex string to a base64 one.
 
   ## Examples
 
-      iex> Cryptopals.hello()
-      :world
+      iex> Cryptopals.hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+      "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 
   """
-  def hello do
-    :world
+  def hex_to_base64(hex_string) do
+    hex_string
+    |> String.replace(~r/\s/, "")
+    |> Base.decode16!(case: :mixed)
+    |> Base.encode64()
   end
 
+  def usage do
+    IO.puts(IO.ANSI.yellow() <> "TODO: USAGE. Check source for now.")
+  end
+
+  @doc """
+  Commandline version of package.
+
+  ## Examples
+
+      mix escript.build && ./cryptopals
+
+  """
   def main(args) do
-    IO.puts "Hello, world!"
+    case args do
+      ["hex_to_base64" | options] ->
+        for input <- options do
+          IO.puts(Cryptopals.hex_to_base64(input))
+        end
+      [unknown | _] ->
+        IO.puts(IO.ANSI.red() <> "error: " <> IO.ANSI.reset() <> "unknown command: #{unknown}.")
+        usage()
+      [] ->
+        usage()
+    end
   end
 end
--- test/cryptopals_test.exs
+++ test/cryptopals_test.exs
@@ -2,7 +2,19 @@ defmodule CryptopalsTest do
   use ExUnit.Case
   doctest Cryptopals
 
-  test "greets the world" do
-    assert Cryptopals.hello() == :world
+  test "Convert hex to base64" do
+    cases = [
+      {
+        "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d",
+        "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+      }
+    ]
+
+    for {input, expected} <- cases do
+      output = Cryptopals.hex_to_base64(input)
+
+      assert output == expected,
+             "Failed for input: #{input}, got: #{output}, expected: #{expected}"
+    end
   end
 end
